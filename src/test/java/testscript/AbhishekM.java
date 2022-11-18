133b9585287abdcb5575e003d636ce268ed55f1d
Abhi-242 <abhishek.mulay.2424@gmail.com>
2022-11-18 15:37:05 +0530

    testing screenshot commit

---
+++ src/test/java/testscript/AbhishekM.java
@@ -0,0 +1,32 @@
+package testscript;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AbhishekM {
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        try
+        {
+        WebDriverManager.chromedriver().setup();  // instead of System.setProperty()
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        
+        File screenShotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenShotFile, new File(".//ScreenShots/screenshotAbhishek.png"));
+        }catch(Exception es) {
+            
+        }
+        
+    }
+
+}
