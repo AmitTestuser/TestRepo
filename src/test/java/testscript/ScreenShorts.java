a51be1e1fc1c8e5b81155e4dd60f34c2a9c07951
ramprasad4455 <ramshinde2.025@gmail.com>
2022-11-18 14:44:02 +0530

    screenshorts file added

---
+++ src/test/java/testscript/ScreenShorts.java
@@ -0,0 +1,33 @@
+package testscript;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScreenShorts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	try {
+//		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.google.com");
+	        driver.manage().window().maximize();
+		
+		File scFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	      FileUtils.copyFile(scFile , new File(".//Screenshots/screen.png"));
+	}
+	catch(Exception ex) {
+		ex.printStackTrace();
+	}
+	}
+
+	
+}
