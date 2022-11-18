69d55bb4a6ab5697327b0be59dab808654b108a5
PrathameshkumbharTesting <prathameshskumbhar.622001@gmail.com>
2022-11-18 15:13:15 +0530

    added new screenshot file which takes screenshot

---
+++ src/test/java/testscript/prathameshScreenshot2.java
@@ -0,0 +1,35 @@
+package testscript;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prathameshScreenshot2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\git project\\TestRepo\\chromedriver.exe");
+		//	WebDriverManager().chromedriver.setup();
+	        try{
+	        	//D:\git project\TestRepo
+	            WebDriver driver= new ChromeDriver();
+	            driver.get("https://google.com");
+	            driver.manage().window().maximize();
+	            File screenShotFile =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	            FileUtils.copyFile(screenShotFile, new File (".//ScreenShots/screen1.png"));
+	            driver.quit();
+	            
+	        }
+	        catch(Exception ex)
+	        {
+	            ex.getStackTrace();
+	            ex.printStackTrace();
+	        }
+			
+	}
+
+}
